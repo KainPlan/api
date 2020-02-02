96649929c06b54f3b61bad42b55a6bc7c27ecf57
MattMoony <monschein.matthias@gmail.com>
2020-02-02 01:52:10 +0100

    documentation; + testing github workflows

--- src/models/KPNode.ts
+++ src/models/KPNode.ts
@@ -5,17 +5,39 @@
 
 import { InvalidNodeFormatError } from "../errors";
 
+/**
+ * Represents a Node stored in the [[KPMap|KainPlan Map]].
+ */
 export default class KPNode {
+  /**
+   * X-Coordinate
+   */
   x: number;
+  /**
+   * Y-Coordinate
+   */
   y: number;
+  /**
+   * Connections to neighbouring nodes.
+   */
   edges: KPNode[];
 
+  /**
+   * Creates a new KPNode with the given parameters.
+   * @param x X-Coordinate
+   * @param y Y-Coordinate
+   * @param edges Connections to other nodes.
+   */
   constructor(x: number, y: number, edges?: KPNode[]) {
     this.x = x;
     this.y = y;
     this.edges = edges || [];
   }
 
+  /**
+   * Creates a new KPNode from the given JSON object/string.
+   * @param json JSON Object/String.
+   */
   static parse(json: any): KPNode {
     if (typeof json === 'string') json = JSON.parse(json);
     if (
@@ -25,6 +47,12 @@ export default class KPNode {
     return new KPNode(json.x, json.y);
   }
 
+  /**
+   * Parses the node's edges from the contents of a JSON structure.
+   * @param json JSON representation of the node.
+   * @param nodes All nodes on the same floor.
+   * @param allNodes All nodes on all floors.
+   */
   parseEdges(json: any, nodes: KPNode[], allNodes?: KPNode[][]): void {
     if (typeof json === 'string') json = JSON.parse(json);
     if (
@@ -34,6 +62,11 @@ export default class KPNode {
     this.edges = json.edges.map((e: number) => nodes[e]);
   }
 
+  /**
+   * Converts the node to its JSON representation. 
+   * @param nodes All nodes on the same floor.
+   * @param allNodes All nodes on all floors.
+   */
   toJSON(nodes: KPNode[], allNodes?: KPNode[][]): any {
     return {
       ...this,
